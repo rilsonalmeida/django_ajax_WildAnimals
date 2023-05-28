d868edfb7dd50c195e17a33651b7c882333d10ca
Rilson Almeida <rilson.almeida@gmail.com>
2023-05-28 11:12:47 -0300

    Javascript behavior in like-unlike buttons

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     post_list_and_create_view,
     load_post_data_view,
+    like_unlike_post_view
 )
 
 app_name = 'posts_app'
 
 urlpatterns = [
     path('', post_list_and_create_view, name='main-page'),
+    path('like-unlike/', like_unlike_post_view, name='like-unlike'),
     path('posts/<int:num_posts>/', load_post_data_view, name='data-posts'),
 ]
--- posts_app/views.py
+++ posts_app/views.py
@@ -18,7 +18,7 @@ def load_post_data_view(request, num_posts):
     data = []
     for obj in qs:
         item = {
-            'id': obj.pk,
+            'id': obj.id,
             'title': obj.title,
             'body': obj.body,
             'liked': True if request.user in obj.liked.all() else False,
@@ -28,3 +28,18 @@ def load_post_data_view(request, num_posts):
         data.append(item)
     return JsonResponse({'data': data[lower:upper], 'size': size})
 
+
+def like_unlike_post_view(request):
+    # if request.is_ajax():  # deprecated funtion since Django 3.1 and removed in Django 4
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
+            
+            
